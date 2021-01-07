77c02bd211d345cd537018fee0a6f99c9d267677
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-01-07 16:42:08 +0100

    aggiunto card

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,5 +4,29 @@
 {{-- con section collego lo yield alla mia homepage in modo da poter inserire il contenuto --}}
 @section('content')
   @include('partials.jumbo')
-<h1>homepage</h1>
+  <section id="series">
+    <div class="container">
+      <div class="section-title">
+        <h2 class="uppercase">current series</h2>
+      </div>
+      <div class="card-container">
+        @foreach ($comics as $item)
+          <div class="card">
+            <div class="comic-img">
+              <img src="{{ $item['thumb'] }}" alt="{{ $item['series'] }}">
+
+            </div>
+            <div class="comic-title">
+              <p class="uppercase">{{ $item['series'] }}</p>
+
+            </div>
+
+          </div>
+        @endforeach
+
+      </div>
+    </div>
+
+  </section>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+  $comics = config('comics');
+  $data = [
+    'comics' => $comics
+  ];
+    return view('homepage', $data);
 })->name('homepage');
 
 Route::get('prodotti', function () {
