b7eabb04468fe173c670a64e20745a89e818b63d
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-01-07 14:50:50 +0100

    inserito header top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,19 @@
 <header>
   <div class="header-top">
-    <nav>
-      <ul>
-        <li>
-          <a href="#">DC Power℠ Visa®</a>
-        </li>
-        <li>
-          <a href="#">Additional DC Sites</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a class="uppercase" href="#">DC Power℠ Visa®</a>
+          </li>
+          <li>
+            <a class="uppercase" href="#">Additional DC Sites</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    </div>
   <div class="header-bottom">
 
   </div>
