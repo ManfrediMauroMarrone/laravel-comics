1280483d942d433f90244d38204e2d7cd4ac8855
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-01-07 15:16:52 +0100

    aggiunto header bot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title>DC Comics</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/43febffcb7.js" crossorigin="anonymous"></script>
   </head>
   <body>
     {{-- con include inserisco in pagina i contenuti dei partials --}}
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,14 +7,60 @@
             <a class="uppercase" href="#">DC Power℠ Visa®</a>
           </li>
           <li>
-            <a class="uppercase" href="#">Additional DC Sites</a>
+            <a class="uppercase" href="#">Additional DC Sites ▼</a>
           </li>
         </ul>
       </nav>
-    </div>
 
     </div>
-  <div class="header-bottom">
+  </div>
 
+  <div class="container">
+    <div class="header-bottom">
+      <div class="logo">
+        <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/logo.png" alt="">
+      </div>
+      <div class="main-menu">
+        <ul>
+          <li>
+            <a href="#">characters</a>
+          </li>
+          <li>
+            <a href="#">comics</a>
+          </li>
+          <li>
+            <a href="#">movies</a>
+          </li>
+          <li>
+            <a href="#">tv</a>
+          </li>
+          <li>
+            <a href="#">games</a>
+          </li>
+          <li>
+            <a href="#">collectibles</a>
+          </li>
+          <li>
+            <a href="#">videos</a>
+          </li>
+          <li>
+            <a href="#">fans</a>
+          </li>
+          <li>
+            <a href="#">news</a>
+          </li>
+          <li>
+            <a href="#">shop</a>
+          </li>
+
+        </ul>
+
+
+      </div>
+
+      <div class="search">
+        <input type="text" name="" value="" placeholder="Search">
+      </div>
+    </div>
   </div>
 </header>
